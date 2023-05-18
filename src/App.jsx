774227aa8536e96f39248893ea20bebc3093db5d
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-05-18 09:12:05 +0600

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/Root.jsx";
 
 const App = () => {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
