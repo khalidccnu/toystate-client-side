91eca3cef085c4d72cf8d073975940b5ca671bf9
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-05-18 09:57:22 +0600

    footer added

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Nav from "../components/Nav.jsx";
+import Footer from "../components/Footer.jsx";
 
 const Root = () => {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <Footer />
+    </>
+  );
 };
 
 export default Root;
